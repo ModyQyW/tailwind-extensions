31463a4791fe9ce3abf707d7b0f5c8d4ddb648a8
ModyQyW <wurui-dev@foxmail.com>
2022-05-03 22:29:40 +0800

    chore: update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   env: {
     node: true,
     commonjs: true,
     es2022: true,
   },
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
 };
