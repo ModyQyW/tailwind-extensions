d71bd55325010b5bd9ef387e62709d720ad29a01
ModyQyW <wurui-dev@foxmail.com>
2022-05-09 19:35:39 +0800

    fix: set darkMediaQuery to empty string

--- src/ant-design.ts
+++ src/ant-design.ts
@@ -59,7 +59,7 @@ export default ({
   disabledText = 'rgba(0, 0, 0, 0.25)',
   boxShadow = '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
   darkSelectors = ['.dark'],
-  darkMediaQuery = '@media (prefers-color-scheme: dark)',
+  darkMediaQuery = '',
   darkPrimary = '#177ddc',
   darkSecondary = '#5a5a5a',
   darkSuccess = '#49aa19',
