26366ec5aca339e189027b520340c1ed2ed7ceef
ModyQyW <wurui-dev@foxmail.com>
2022-06-10 21:55:07 +0800

    feat: add types

---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+import * as Color from 'color';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ColorParam = Color | string | ArrayLike<number> | number | { [key: string]: any };
+
+export type { Config } from 'tailwindcss';
