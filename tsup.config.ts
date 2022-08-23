f75f21ee1c4404c6845c305adda52147af4c17c3
ModyQyW <wurui-dev@foxmail.com>
2022-08-23 09:38:47 +0800

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -26,19 +26,26 @@ const getTsFiles = (entry: string): string[] =>
 export default defineConfig([
   {
     entry: getTsFiles('src'),
-    dts: true,
     format: 'esm',
+    dts: true,
     minify: true,
+    shims: true,
     splitting: false,
     target: 'node12',
     banner: {
-      js: `import {createRequire} from 'module';var require=createRequire(import\.meta.url);`,
+      js: `import {createRequire as __createRequire} from 'module';var require=createRequire(import\.meta.url);`,
     },
   },
   {
     entry: getTsFiles('src'),
     format: 'cjs',
     minify: true,
+    shims: true,
+    splitting: false,
     target: 'node12',
+    footer: {
+      js: `if (module.exports.default) module.exports = module.exports.default;`,
+    },
   },
 ]);
+/* eslint-enable no-useless-escape */
