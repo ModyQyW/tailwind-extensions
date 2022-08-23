1dd87fe76e1ebc6981f5654a6596b4533d39f3f5
ModyQyW <wurui-dev@foxmail.com>
2022-08-23 11:36:21 +0800

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,31 +1,9 @@
 /* eslint-disable no-useless-escape */
 import { defineConfig } from 'tsup';
-import fs from 'node:fs';
-import path from 'node:path';
-
-const getTsFiles = (entry: string): string[] =>
-  fs
-    .readdirSync(entry)
-    .flatMap((item) => {
-      if (
-        item.includes('.ts') &&
-        !item.includes('.test.') &&
-        !item.includes('.spec.') &&
-        !item.includes('.d.') &&
-        !item.includes('type')
-      ) {
-        return path.join(entry, item);
-      }
-      if (!item.includes('.') && !item.includes('type')) {
-        return getTsFiles(path.join(entry, item));
-      }
-      return '';
-    })
-    .filter((item) => !!item) as string[];
 
 export default defineConfig([
   {
-    entry: getTsFiles('src'),
+    entry: ['src/index.ts'],
     format: 'esm',
     dts: true,
     minify: true,
@@ -37,7 +15,7 @@ export default defineConfig([
     },
   },
   {
-    entry: getTsFiles('src'),
+    entry: ['src/index.ts'],
     format: 'cjs',
     minify: true,
     shims: true,
