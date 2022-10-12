e2ee8ccf37eede9d0405872c31386dd4eb16214f
ModyQyW <wurui-dev@foxmail.com>
2022-10-12 17:03:44 +0800

    fix: fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,18 +4,17 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: ['src/index.ts'],
+    clean: true,
     format: 'esm',
     dts: true,
     minify: true,
     shims: true,
     splitting: false,
     target: 'node14.16',
-    banner: {
-      js: `import {createRequire as __createRequire} from 'module';var require=createRequire(import\.meta.url);`,
-    },
   },
   {
     entry: ['src/index.ts'],
+    clean: true,
     format: 'cjs',
     minify: true,
     shims: true,
