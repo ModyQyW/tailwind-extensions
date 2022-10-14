64b1fbfddd9310d9ae07d285dcdd52a46e2a71cd
ModyQyW <wurui-dev@foxmail.com>
2022-10-14 13:31:10 +0800

    feat!: remove miniprogramScreensPreset

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -2,5 +2,4 @@ export * from './base';
 export * from './ant-design';
 export * from './element-plus';
 export * from './miniprogram-base';
-export * from './miniprogram-screens';
 export * from './easy';
--- src/presets/miniprogram-screens.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from 'tailwindcss';
-
-export const miniprogramScreensPreset: Partial<Config> = {
-  theme: {
-    screens: {},
-  },
-};
