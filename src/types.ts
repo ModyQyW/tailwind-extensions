6b2009c2eb3415a3a14c696e8e99339893dd9b36
ModyQyW <wurui-dev@foxmail.com>
2022-10-14 10:48:57 +0800

    chore: remove types

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import type Color from 'color';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ColorParam = Color | string | ArrayLike<number> | number | { [key: string]: any };
-
-import type { Config as TailwindcssConfig } from 'tailwindcss';
-
-export type Config = Partial<TailwindcssConfig>;
