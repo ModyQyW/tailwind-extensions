091b6af4baeae2b4f57571c356357743eb52ee5a
ModyQyW <wurui-dev@foxmail.com>
2022-10-14 11:59:26 +0800

    fix: fix exports

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -3,5 +3,4 @@ export * from './ant-design';
 export * from './element-plus';
 export * from './miniprogram-base';
 export * from './miniprogram-screens';
-export * from './miniprogram-separator';
 export * from './easy';
