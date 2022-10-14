927a41f70c14162bcf299e4323746375e6c41373
ModyQyW <wurui-dev@foxmail.com>
2022-10-14 10:16:45 +0800

    feat!: update exports

---
+++ src/presets/index.ts
@@ -0,0 +1,7 @@
+export * from './base';
+export * from './ant-design';
+export * from './element-plus';
+export * from './miniprogram-base';
+export * from './miniprogram-screens';
+export * from './miniprogram-separator';
+export * from './easy';
