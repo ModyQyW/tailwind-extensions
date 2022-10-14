c447e1793251ce951067e1f2edb9c91897841295
ModyQyW <wurui-dev@foxmail.com>
2022-10-14 10:41:34 +0800

    chore: migrate miniprogram-screens

--- src/miniprogram-screens.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from './types';
-
-const config: Config = {
-  theme: {
-    screens: {},
-  },
-};
-
-export default config;
---
+++ src/presets/miniprogram-screens.ts
@@ -0,0 +1,7 @@
+import { Config } from 'tailwindcss';
+
+export const miniprogramScreensPreset: Partial<Config> = {
+  theme: {
+    screens: {},
+  },
+};
