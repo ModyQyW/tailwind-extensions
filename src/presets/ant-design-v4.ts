a78a03d3c9f96df3b34c4cddf366ba178e675dbf
ModyQyW <wurui-dev@foxmail.com>
2022-12-21 10:54:32 +0800

    feat!: antDesignPreset -> antDesignV4Preset

--- src/presets/ant-design-v4.ts
+++ src/presets/ant-design-v4.ts
@@ -39,7 +39,7 @@ const convert = (colors: string[]) => {
   };
 };
 
-export const antDesignPreset = ({
+export const antDesignV4Preset = ({
   /** Base */
   baseSelectors = [':root'],
   baseMediaQuery = '',
--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,5 +1,5 @@
 export * from './base';
-export * from './ant-design';
+export * from './ant-design-v4';
 export * from './element-plus';
 export * from './miniprogram-base';
 export * from './easy';
